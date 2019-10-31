1474274543f9460669a70db102c4c24fb81d8b22
Thomas Francis Jr <franc@tcsaf.com>
2019-10-31 18:29:05 -0400

    Updated Dockerfile for armhf builds

--- snips-base/start-arm-build.sh
+++ snips-base/start-arm-build.sh
@@ -1,28 +1,44 @@
-if [ "${BUILD_ARCH}" != "armhf" ] ; then
-    exit 0
-fi
-
-/usr/bin/qemu-arm-static /bin/cp /bin/sh /bin/sh.real
-/usr/bin/qemu-arm-static /bin/cp /bin/bash /bin/bash.real
+#!/usr/bin/qemu-arm-static /bin/sh
 
 /usr/bin/qemu-arm-static /bin/cat >/bin/sh-shim <<'_EOF'
 #!/usr/bin/qemu-arm-static /bin/sh.real
 set -o errexit
 
-cp /bin/sh.real /bin/sh
-/bin/sh "$@"
-cp /usr/bin/sh-shim /bin/sh
+/usr/bin/qemu-arm-static /bin/rm -f /bin/sh
+/usr/bin/qemu-arm-static /bin/ln -s /bin/sh.real /bin/sh
+/usr/bin/qemu-arm-static /bin/echo /bin/sh -c \"$@\"
+/usr/bin/qemu-arm-static -execve /bin/sh -c "$@"
+/usr/bin/qemu-arm-static /bin/echo done with real sh run
+/usr/bin/qemu-arm-static /bin/rm -f /bin/sh
+/usr/bin/qemu-arm-static /bin/ln -s /bin/sh-shim /bin/sh
+/usr/bin/qemu-arm-static /bin/ls -lF /bin/sh
 _EOF
+
 /usr/bin/qemu-arm-static /bin/cat >/bin/bash-shim <<'_EOF'
 #!/usr/bin/qemu-arm-static /bin/bash.real
+set -x
 set -o errexit
 
-cp /bin/bash.real /bin/bash
-/bin/bash "$@"
-cp /usr/bin/bash-shim /bin/bash
+/usr/bin/qemu-arm-static /bin/rm -f /bin/bash
+/usr/bin/qemu-arm-static /bin/ln -s /bin/bash.real /bin/bash
+/usr/bin/qemu-arm-static /bin/echo temp call to apt-get update
+/usr/bin/qemu-arm-static -execve /usr/bin/apt-get update
+/usr/bin/qemu-arm-static /bin/echo END temp call to apt-get update
+/usr/bin/qemu-arm-static /bin/echo /bin/bash -c \"$@\"
+/usr/bin/qemu-arm-static -execve /bin/bash -c "$@"
+/usr/bin/qemu-arm-static /bin/echo done with real bash run
+/usr/bin/qemu-arm-static /bin/rm -f /bin/bash
+/usr/bin/qemu-arm-static /bin/ln -s /bin/bash-shim /bin/bash
+/usr/bin/qemu-arm-static /bin/ls -lF /bin/bash
 _EOF
-/usr/bin/qemu-arm-static /bin/chmod 755 /bin/sh-shim /usr/bin/qemu-arm-static
-/usr/bin/qemu-arm-static /bin/chmod 755 /bin/bash-shim /usr/bin/qemu-arm-static
 
-/usr/bin/qemu-arm-static /bin/cp /bin/sh-shim /bin/sh
-/usr/bin/qemu-arm-static /bin/cp /bin/bash-shim /bin/bash
+/usr/bin/qemu-arm-static /bin/mv /bin/sh /bin/sh.real
+/usr/bin/qemu-arm-static /bin/mv /bin/bash /bin/bash.real
+
+/usr/bin/qemu-arm-static /bin/chmod 755 /bin/sh-shim
+/usr/bin/qemu-arm-static /bin/rm -f /bin/sh
+/usr/bin/qemu-arm-static /bin/ln -s /bin/sh-shim /bin/sh
+
+/usr/bin/qemu-arm-static /bin/chmod 755 /bin/bash-shim
+/usr/bin/qemu-arm-static /bin/rm -f /bin/bash
+/usr/bin/qemu-arm-static /bin/ln -s /bin/bash-shim /bin/bash
--- snips-base/stop-arm-build.sh
+++ snips-base/stop-arm-build.sh
@@ -1,18 +1,13 @@
-#!/bin/sh
+#!/usr/bin/qemu-arm-static /bin/sh
+/usr/bin/qemu-arm-static /bin/rm -f /bin/sh
+/usr/bin/qemu-arm-static /bin/mv /bin/sh.real /bin/sh
+/usr/bin/qemu-arm-static /bin/rm -f /bin/sh-shim
 
-if [ "${BUILD_ARCH}" == "armhf" ] ; then
-    /usr/bin/qemu-arm-static /bin/cp /bin/sh.real /bin/sh
-    /usr/bin/qemu-arm-static /bin/rm -f /bin/sh-shim
-    /usr/bin/qemu-arm-static /bin/cp /bin/bash.real /bin/bash
-    /usr/bin/qemu-arm-static /bin/rm -f /bin/bash-shim
-    /usr/bin/qemu-arm-static /bin/rm -f /usr/bin/qemu-arm-static
-    /usr/bin/qemu-arm-static /bin/rm -f /start-arm-build.sh
-    /usr/bin/qemu-arm-static /bin/rm -f /stop-arm-build.sh
-else
-    rm -f /start-arm-build.sh
-    rm -f /stop-arm-build.sh
-fi
+/usr/bin/qemu-arm-static /bin/rm -f /bin/bash
+/usr/bin/qemu-arm-static /bin/mv /bin/bash.real /bin/bash
+/usr/bin/qemu-arm-static /bin/rm -f /bin/bash-shim
 
-exit 0
-
+/usr/bin/qemu-arm-static /bin/rm -f /usr/bin/qemu-arm-static
+/usr/bin/qemu-arm-static /bin/rm -f /start-arm-build.sh
+/usr/bin/qemu-arm-static /bin/rm -f /stop-arm-build.sh
 
